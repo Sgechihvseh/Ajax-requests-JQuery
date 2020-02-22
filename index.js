42ee430f6ef0a8d29355d411e115669c5be10607
Рома Хмарук <rkhmaruk4@gmail.com>
2020-02-22 19:19:41 +0300

    Ajax-request

---
+++ index.js
@@ -0,0 +1,17 @@
+const resultBlock = document.querySelector("#result");
+const clickMeButton = document.querySelector("#click-me");
+const pageNumber = document.querySelector("#page-number");
+clickMeButton.addEventListener("click", makeRequest);
+
+function makeRequest() {
+	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
+		success: function (data) {
+				data.forEach(el => {
+					const img = document.createElement('img');
+					img.src = el.thumbnail;
+					document.querySelector('#result').appendChild(img);
+				});
+
+			}
+	})
+}
