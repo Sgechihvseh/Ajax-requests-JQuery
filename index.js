2361b4051ac0b773ed27aeda9e1683761a9f79ce
Рома Хмарук <rkhmaruk4@gmail.com>
2020-02-22 23:21:09 +0300

    Refactoring. Add callback's

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const resultBlock = document.querySelector("#result");
 const clickMeButton = document.querySelector("#click-me");
 const pageNumber = document.querySelector("#page-number");
-clickMeButton.addEventListener("click", makeRequest);
+clickMeButton.addEventListener("click", () => {getImages(pageNumber.value, onDataReceived)} );
 
-function makeRequest() {
-	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
-		success: function (data) {
-				data.forEach(el => {
-					const img = document.createElement('img');
-					img.src = el.thumbnail;
-					document.querySelector('#result').appendChild(img);
-				});
-
-			}
-	})
+	function onDataReceived (data) {
+		data.forEach(el => {
+			const img = document.createElement('img');
+			img.src = el.thumbnail;
+			document.querySelector('#result').appendChild(img);
+		});
 }
+
