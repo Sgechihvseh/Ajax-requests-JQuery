588a3c9866acb97649de0a354cbfa825d5871dba
Рома Хмарук <rkhmaruk4@gmail.com>
2020-02-29 15:25:01 +0300

    Add new functionality (post task, get task, delete task)

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
-const resultBlock = document.querySelector("#result");
+const resultBlockImg = document.querySelector("#resultimg");
+const resultBlockTask = document.querySelector("#resulttask");
+
 const clickMeButton = document.querySelector("#click-me");
-const pageNumber = document.querySelector("#page-number");
+const addTaskButton = document.querySelector("#addtask");
+const showTaskButton = document.querySelector("#showtask");
+const deleteTaskButton = document.querySelector("#deletetask");
+
+const pageNumber = document.querySelector("#numberimg");
+const taskText = document.querySelector("#tasktext");
+
 clickMeButton.addEventListener("click", () => {
 	const promise = getImages(pageNumber.value);
-	promise.then(onDataReceived)
+	promise.then(onGetImages)
+});
+
+addTaskButton.addEventListener("click", () => {
+	const promise = postTasks(taskText.value);
+	promise.then(renderTask);
 } );
 
-	function onDataReceived (data) {
-		data.forEach(el => {
-			const img = document.createElement('img');
-			img.src = el.thumbnail;
-			document.querySelector('#result').appendChild(img);
-		});
+showTaskButton.addEventListener("click", () => {
+	const promise = getTasks();
+	promise.then(onGetTasks);
+});
+
+function renderTask() {
+	const promise = getTasks();
+	promise.then(onGetTasks);
+};
+
+deleteTaskButton.addEventListener("click", () => {
+	deleteTask('5220a757-0e83-4af8-b328-42d65ce2e232');
+})
+
+function onGetImages(data) {
+	data.forEach(el => {
+		const img = document.createElement('img');
+		img.src = el.thumbnail;
+		document.querySelector('#resultimg').appendChild(img);
+	});
+
+}
+function onGetTasks(tasks) {
+	const result = document.querySelector('#resulttask');
+	result.innerHTML = "";
+	tasks.forEach(task => {
+		const li = document.createElement('li');
+		li.innerHTML = task.title;
+		li.dataset.id = task.id;
+		result.appendChild(li);
+	});
 }
 
--- service.js
+++ service.js
@@ -1,7 +1,35 @@
+
 function getImages(pageNumber) {
 	const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`)
 
-	return promise.then((data) => {
-		return data.data;
+	return promise.then((response) => {
+		return response.data;
+	});
+}
+
+function postTasks(task) {
+	const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
+		widgetId: 543265,
+		title: task
+	})
+
+	return promise.then((response) => {
+		return response.data;
+	});
+}
+
+function getTasks() {
+	const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=543265&count=30`)
+
+	return promise.then((response) => {
+		return response.data;
+	});
+}
+
+function deleteTask(id) {
+	const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetId=543265&taskId=${id}`)
+
+	return promise.then((response) => {
+		return response.data;
 	});
 }
